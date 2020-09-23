51be7e66e209494a0a3df8a44ef669a6a786e4a4
pedrammgd <pedram.mojarad77@gmail.com>
2020-09-23 19:00:04 +0330

    3commit

--- app/src/main/java/com/example/huawei1/MainActivity.java
+++ app/src/main/java/com/example/huawei1/MainActivity.java
@@ -3,19 +3,48 @@ package com.example.huawei1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String ACTIVITY = "Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
-
+        Log.e(ACTIVITY,"onCreate");
     }
-    public void ali(){
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("Activity","onStart");
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("Activity","onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("Activity","onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("Activity","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("Activity","onDestroy");
+    }
+
 }
