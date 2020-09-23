5d1b1f08fb54e5ce87522e54991d3f251dccfe4d
pedrammgd <pedram.mojarad77@gmail.com>
2020-09-23 19:14:43 +0330

    pedyjoon

--- app/src/main/java/com/example/huawei1/MainActivity.java
+++ app/src/main/java/com/example/huawei1/MainActivity.java
@@ -15,45 +15,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 //         pedram
-        Log.e(ACTIVITY,"onCreate");
+//        fdgdfgdfgdf
+        Log.e(ACTIVITY, "onCreate");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.e("Activity","onStart");
+        Log.e("Activity", "onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.e("Activity","onResume");
+        Log.e("Activity", "onResume");
     }
 
-<<<<<<< HEAD
+
     @Override
     protected void onPause() {
         super.onPause();
-        Log.e("Activity","onPause");
+        Log.e("Activity", "onPause");
     }
-=======
-//         System.out.print("pedram");
->>>>>>> 53c16a8748b4cb08f1cc685c821402330f82cb51
+
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.e("Activity","onStop");
+        Log.e("Activity", "onStop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.e("Activity","onDestroy");
+        Log.e("Activity", "onDestroy");
     }
-<<<<<<< HEAD
-
 }
-=======
-}
->>>>>>> 53c16a8748b4cb08f1cc685c821402330f82cb51
