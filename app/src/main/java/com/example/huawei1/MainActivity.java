8f3bea22a673f4fe13674555a2d57007e9e5954d
pedrammgd <pedram.mojarad77@gmail.com>
2020-09-28 15:38:47 +0330

    commit color

--- app/src/main/java/com/example/huawei1/MainActivity.java
+++ app/src/main/java/com/example/huawei1/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.huawei1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String ACTIVITY = "Activity";
+
+    public static final String MYLOG = "Mylog";
+    public static final String NAME = "name";
+    public static final String Last = "lastN";
+
+
+
+    TextView textView,textView2;
+    Button button,button2;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,38 +32,89 @@ public class MainActivity extends AppCompatActivity {
 //         pedram
 //        fdgdfgdfgdf
 //         1221222121
-        Log.e(ACTIVITY, "onCreate");
+
+
+
+        editText = findViewById(R.id.editText);
+
+        button = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+
+        textView = findViewById(R.id.textView1);
+        textView2 = findViewById(R.id.textView2);
+
+
+
+        button.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                intent.putExtra(NAME,"pedram");
+                intent.putExtra(Last,"mojarad");
+
+
+                startActivity(intent);
+            }
+        });
+
+
+        button2.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                textView2.setText(textView.getText());
+                return false;
+            }
+        });
+
+
+
+
+
+
+
+        Log.e(MYLOG,"onCreate");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.e("Activity", "onStart");
+        Log.e(MYLOG,"onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.e("Activity", "onResume");
+        Log.e(MYLOG,"onResume");
     }
-
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.e("Activity", "onPause");
+        Log.e(MYLOG,"onPause");
     }
-
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.e("Activity", "onStop");
+        Log.e(MYLOG,"onStop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.e("Activity", "onDestroy");
+        Log.e(MYLOG,"onDestroy");
+    }
+
+    public void btn1(View view) {
+        Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+        String email = editText.getText().toString();
+        intent.putExtra("email",email);
+        startActivity(intent);
+    }
+
+    public void google(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("h"));
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/huawei1/MainActivity2.java
+++ app/src/main/java/com/example/huawei1/MainActivity2.java
@@ -2,9 +2,15 @@ package com.example.huawei1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    TextView textView,textView3,textView4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +19,32 @@ public class MainActivity2 extends AppCompatActivity {
 //         pedram
 //        aaaaaa
 //         bbbbb
+
+        textView = findViewById(R.id.textView);
+        textView3 = findViewById(R.id.textView3);
+        textView4 = findViewById(R.id.textView4);
+
+
+        Bundle bundle = getIntent().getExtras();
+
+        if (bundle == null) {
+
+
+            return;
+        } else {
+            String firstname = bundle.getString(MainActivity.NAME);
+            String lastname = bundle.getString(MainActivity.Last);
+
+            String email = bundle.getString("email");
+
+
+            textView4.setText(email);
+            textView.setText(firstname);
+            textView3.setText(lastname);
+        }
+    }
+    public void btnBack(View view) {
+        Intent intent = new Intent(MainActivity2.this,MainActivity.class);
+        startActivity(intent);
     }
 }
